64a6521bff9abca23ba2bf93a007cb9cc78f8889
张犁宝 <353227876@qq.com>
2023-04-20 13:48:00 +0800

    代理小工具

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Windows;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace myNetwork
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty OpenCommandProperty =
+         DependencyProperty.Register("OpenCommand", typeof(RoutedCommand), typeof(MainWindow), new PropertyMetadata(null));
+        public static string proxyServerAddress = "127.0.0.1";
+        public static string proxyServerPort = "10809";
+        private Process proIpv4 = null;
+        private Process proIpv6 = null;
+        private string netmodle = "ipv4";
+        public RoutedCommand OpenCommand
+        {
+            get { return (RoutedCommand)GetValue(OpenCommandProperty); }
+            set { SetValue(OpenCommandProperty, value); }
+        }
+         //static NetworkTools mynetworkTools = new NetworkTools();
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        /// <summary>
+        /// 选中事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BulletCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            //打开代理
+            NetworkTools myNetWorkTools = new NetworkTools();
+
+            myNetWorkTools.SetProxy(true, proxyServerAddress, proxyServerPort);
+            if (netmodle == "ipv4")
+            {
+                if (proIpv4 == null)
+                {
+                    ProcessStartInfo info = new ProcessStartInfo();
+                    string filenme = Directory.GetCurrentDirectory() + @"\IPv4\xrayIpv4.exe";
+                    info = myNetWorkTools.startmyexe(filenme);
+                    proIpv4 = Process.Start(info);
+                }
+            }
+            else
+            {
+                if (proIpv6 == null)
+                {
+                    ProcessStartInfo info = new ProcessStartInfo();
+                    string filenme = Directory.GetCurrentDirectory() + @"\IPv6\xrayIpv6.exe";
+                    info = myNetWorkTools.startmyexe(filenme);
+                    proIpv6 = Process.Start(info);
+                }
+            }
+        }
+        /// <summary>
+        /// 取消选择事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BulletCheckBox_unChecked(object sender, RoutedEventArgs e)
+        {
+            //关闭代理
+            NetworkTools myNetWorkTools = new NetworkTools();
+
+            myNetWorkTools.SetProxy(false, proxyServerAddress, proxyServerPort);
+            if (proIpv4 != null)
+            {
+                proIpv4.Kill();
+                proIpv4.WaitForExit();
+                proIpv4 = null;
+            }
+
+            if(proIpv6 != null)
+            {
+                proIpv6.Kill();
+                proIpv6.WaitForExit();
+                proIpv6 = null;
+            }
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton btn = sender as RadioButton;
+            if (btn == null)
+                return;
+            NetworkTools myNetWorkTools = new NetworkTools();
+            string filepath = "";
+            bool proxyFlag = myNetWorkTools.GetProxy();
+            if (!proxyFlag){
+                return;
+            }
+            myNetWorkTools.SetProxy(true, proxyServerAddress, proxyServerPort);
+            string rootpath = Directory.GetCurrentDirectory();
+            string cloasepath = "";
+            if (btn.Name == "IPv4")
+            {
+                this.netmodle = "ipv4";
+                if (proIpv6 != null)
+                {
+                    proIpv6.Kill();
+                    proIpv6.WaitForExit();
+                    proIpv6 = null;
+                }
+                if (proIpv4 == null)
+                {
+                    ProcessStartInfo info = new ProcessStartInfo();
+                    string filenme = Directory.GetCurrentDirectory() + @"\IPv4\xrayIpv4.exe";
+                    info = myNetWorkTools.startmyexe(filenme);
+                    proIpv4 = Process.Start(info);
+                }
+
+            }
+            if (btn.Name == "IPv6")
+            {
+                this.netmodle = "ipv6";
+                
+                if (proIpv4 != null)
+                {
+                    proIpv4.Kill();
+                    proIpv4.WaitForExit();
+                    proIpv4 = null;
+                }
+                if (proIpv6 == null)
+                {
+                    string filenme = Directory.GetCurrentDirectory() + @"\IPv6\xrayIpv6.exe";
+                    ProcessStartInfo info6 = myNetWorkTools.startmyexe(filenme);
+                    //info = myNetWorkTools.startmyexe(filenme);
+                    proIpv6 = Process.Start(info6);
+                }
+            }
+
+            /*  Task task = Task.Run(() =>
+              {
+                  myNetWorkTools.closemyexe(cloasepath);
+                  myNetWorkTools.startmyexe(filepath);
+              });*/
+
+        }
+        /// <summary>
+        /// 窗口移动事件
+        /// </summary>
+        private void TitleBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+        /// <summary>
+        /// 窗口关闭
+        /// </summary>
+        private void btn_close_Click(object sender, RoutedEventArgs e)
+        {
+            //关闭代理
+            NetworkTools myNetWorkTools = new NetworkTools();
+            myNetWorkTools.SetProxy(false, proxyServerAddress, proxyServerPort);
+            if (proIpv4 != null)
+            {
+                proIpv4.Kill();
+                proIpv4.WaitForExit();
+                proIpv4 = null;
+            }
+
+            if (proIpv6 != null)
+            {
+                proIpv6.Kill();
+                proIpv6.WaitForExit();
+                proIpv6 = null;
+            }
+            this.Close();
+        }
+
+        /// <summary>
+        /// 窗口最小化
+        /// </summary>
+        private void btn_min_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; //设置窗口最小化
+        }
+    }
+}
---
+++ NetworkTools.cs
@@ -0,0 +1,104 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace myNetwork
+{
+    internal class NetworkTools
+    {
+        public NetworkTools()
+        {
+
+        }
+
+
+        public void SetProxy(bool bOpen, string proxyServerAddress,string proxyServerPort)
+        {
+            //打开注册表
+            var regKey = Registry.CurrentUser;
+            const string subKeyPath = "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
+            var optionKey = regKey.OpenSubKey(subKeyPath, true);
+            //更改健值，设置代理，
+            if (optionKey != null)
+            {
+                optionKey.SetValue("ProxyEnable", bOpen ? 1 : 0);
+
+                optionKey.SetValue("ProxyServer", (proxyServerAddress + ":" + proxyServerPort));
+            }
+            //激活代理设置
+            InternetSetOption(0, 39, IntPtr.Zero, 0);
+            InternetSetOption(0, 37, IntPtr.Zero, 0);
+        }
+        // DllImport：动态端口
+        [DllImport(@"wininet",
+        SetLastError = true,
+        CharSet = CharSet.Auto,
+        EntryPoint = "InternetSetOption",
+        CallingConvention = CallingConvention.StdCall)]
+        // InternetSetOption：互联网设置选项
+        public static extern bool InternetSetOption
+        (
+            int hInternet,
+            int dmOption,
+            IntPtr lpBuffer,
+            int dwBufferLength
+         );
+        // 获取代理
+        public bool GetProxy()
+        {
+            //打开注册表CurrentUser
+            var regKey = Registry.CurrentUser;
+
+            const string subKeyPath = "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings";
+            var optionKey = regKey.OpenSubKey(subKeyPath, true);
+            //更改健值，设置代理
+            if (optionKey != null)
+            {
+                // 获取是否使用代理服务器
+                string proxyEnable = optionKey.GetValue("ProxyEnable").ToString();
+                // 获取地址跟端口
+                string addressAndPort = optionKey.GetValue("ProxyServer").ToString();
+                return proxyEnable == "1" ? true : false;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        //启动exe程序
+        public ProcessStartInfo startmyexe(string filepath)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.FileName = filepath;
+            info.Arguments = "";
+            info.WindowStyle = ProcessWindowStyle.Hidden;
+            info.UseShellExecute = false;
+            info.RedirectStandardOutput = true;
+            info.RedirectStandardInput = true;
+            info.RedirectStandardError = true;
+            info.CreateNoWindow = true;
+            return info;
+            
+            //pro.WaitForExit();  
+        }
+        public void closemyexe(string name)
+        {
+            Process[] p_arry = Process.GetProcesses();//得到系统所有进程
+            for (int i = 0; i < p_arry.Length; i++)//遍历每个进程
+            {
+                if (p_arry[i].ProcessName == name)//发现有名为QQ的进程
+                {
+                    p_arry[i].Kill();//就结束它。
+                    return;
+                }
+            }
+            System.GC.Collect();//垃圾回收
+        }
+
+
+    }
+}
